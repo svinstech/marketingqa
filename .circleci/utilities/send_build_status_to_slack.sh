04679a66c06b1d4a55ed807aee367d47513db367
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-02-02 15:58:20 -0500

    Put the whole message on 1 line.

--- .circleci/utilities/send_build_status_to_slack.sh
+++ .circleci/utilities/send_build_status_to_slack.sh
@@ -142,8 +142,8 @@ RUN_URL="https://dashboard.cypress.io/#/projects/iukrxp/runs"
 # done < "${input}"
 
 # build the message content
-MESSAGE="${VOUCH_ICON} marketingqa publish_site test status: ${SLACK_BUILD_STATUS}! <${RUN_URL}|Cypress Dashboard> | <${CIRCLE_BUILD_URL}|CircleCI Job>\n"
-MESSAGE+="<${ORIGINATING_BUILD_URL}|marketingqa job run> for git branch ${ORIGINATING_BRANCH}\n"
+MESSAGE="${VOUCH_ICON} marketingqa publish_site test status: ${SLACK_BUILD_STATUS}! <${RUN_URL}|Cypress Dashboard> | <${CIRCLE_BUILD_URL}|CircleCI Job>\n<${ORIGINATING_BUILD_URL}|marketingqa job run> for git branch ${ORIGINATING_BRANCH}\n"
+### MESSAGE+="<${ORIGINATING_BUILD_URL}|marketingqa job run> for git branch ${ORIGINATING_BRANCH}\n"
 ### MESSAGE+="(<${COMMIT_URL:-unset}|${SHORT_SHA1}> by ${SLACK_NOTIFY}) ${SHORT_GIT_MESSAGE}\n"
 
 # and add PR to message if available
