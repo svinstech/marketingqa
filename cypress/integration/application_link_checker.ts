0fe674ea9185fe607de219f9b2c65581eb55a210
Kellen Kincaid <kellen.kincaid@vouch.us>
2023-02-03 13:48:05 -0500

    Added helpful comments

--- cypress/integration/application_link_checker.ts
+++ cypress/integration/application_link_checker.ts
@@ -10,7 +10,6 @@ const shuffle :any = require('shuffle-array');
 const baseUrl :string|null = Cypress.config('baseUrl')
 import { companyUrlObject, GetUpdatedUrlList } from "../functions/link_checker_functions";
 
-
 // Master array for URL objects.
 let urlObjects :companyUrlObject[]; // companyUrlObjects for each relevant URL.
 
@@ -46,124 +45,9 @@ for (let i :number = 0; i < premierPartnerPagesKeys.length; i++) {
     premierPartnerUrlObjects.push(companyDetails);
 }
 
-
-
-/*
-    [ARGS]
-    _targetUrlObject   =   A companyUrlObject representing a url and a company name.
-                            This URL will be visited and have its application link tested.
-                            The test will ensure that clicking the link leads to the application page.
-
-    NOTE:
-        undefined is also an expected type for _targetUrlObject.
-        This is for the edge case where the array that is invoking this function on its contents
-            tries to invoke it on an element that doesn't exist (index out-of-bounds).
-*/
-// function VerifyApplyButtonWorks(_targetUrlObject :companyUrlObject|undefined) {
-//     if (_targetUrlObject) {
-//         // Go to the target URL.
-//         cy.visit(_targetUrlObject.url);
-
-//         // Returning false here prevents Cypress from failing the test on uncaught exceptions.
-//         Cypress.on('uncaught:exception', () => { return false })
-
-//         /*
-//             According to Gabe Tiger, the 'apply-trigger' class is supposed to ONLY be on the links that lead to pages
-//                 with the app.vouch.us domain.
-//             If you find that a link has this class and does not lead to that domain, it is likely a bug.
-//         */
-//         const applyLinkSelector :string = 'a.apply-trigger';
-
-//         // Test ALL application buttons
-//         cy.get(applyLinkSelector)
-//         .then(($elements :JQuery<HTMLElement>) => {
-//             const applyLinkCount :number = $elements.length;
-
-//             // Log the total number of applicaition links on this page.
-//             cy.log(`NUMBER OF APPLICATION LINKS: ${applyLinkCount}`);
-
-//             for (let i = 0; i < applyLinkCount; i++) {
-//                 // Click the ith application button.
-//                 /*
-//                     NOTE:
-//                         Here, we do the exact same filtering as above because when we navigate away
-//                         from this page, the connection to all DOM elements on the page that we left
-//                         is gone.
-//                 */
-//                 cy.get(applyLinkSelector)
-//                 .eq(i).click();
-
-//                 cy.url().then(_url => {
-//                     const vouchGetStartedUrl :string = 'www.vouch.us/getstarted';
-//                     const currentlyOnTheGetStartedUrl = _url.includes(vouchGetStartedUrl);
-
-
-//                     if (currentlyOnTheGetStartedUrl) {
-//                         ///// !START! vouch.us/getstarted edge case !START! /////
-//                         /*
-//                             If clicking the application link takes us to the vouch.us/getstarted page,
-//                                 then we must click the application link on that page to finally reach
-//                                 the app.vouch.us/...
-//                         */
-//                         cy.get(applyLinkSelector).then(($elements_getStartedPage :JQuery<HTMLElement>) => {
-//                             const applyLinkCount_getStartedPage :number = $elements_getStartedPage.length;
-
-//                             // Log the total number of applicaition links on the getStarted page.
-//                             cy.log(`NUMBER OF APPLICATION LINKS (getStarted): ${applyLinkCount_getStartedPage}`);
-
-//                             for (let j = 0; j < applyLinkCount; j++) {
-//                                 // Click the ith application button.
-//                                 cy.get(applyLinkSelector)
-//                                 .eq(j).click();
-
-//                                 ValidateApplicationPage(_targetUrlObject);
-//                             }
-//                         })
-//                         ///// !END!   vouch.us/getstarted edge case   !END! /////
-//                     } else {
-//                         ValidateApplicationPage(_targetUrlObject);
-//                     }
-//                 })
-//             }
-//         })
-//     } else { 
-//         cy.log(`Url object is undefined.`);
-//         // skipOn(!_targetUrlObject)
-//     }
-// }
-
-/*
-    [ARGS]
-    _targetUrlObject        =   A companyUrlObject representing a url and a company name.
-    _returnToOriginalUrl    =   Optional boolean. Defaults to true.
-                                 If true, then it will go to the URL of _targetUrlObject at the end of the function.
-*/
-// function ValidateApplicationPage(_targetUrlObject :companyUrlObject, _returnToOriginalUrl :boolean = true) {
-//     // Ensure that the resulting URL has the correct domain.
-//     const vouchApplyDomain :string = 'https://app.vouch.us/';
-//     cy.url().should('contain', vouchApplyDomain);
-
-//     // Ensure that the resulting URL has the correct partenr slug.
-//     cy.url().then(_url => {
-//         const urlPartnerSlug :string = `partner=${_targetUrlObject.companyName}`;
-
-//         // Debugging
-//         const urlContainsPartnerName :boolean = _url.includes(urlPartnerSlug);
-//         if (!urlContainsPartnerName) {
-//             cy.log(`~~! EXPECTED URL TO CONTAIN: ${urlPartnerSlug}`);
-//             cy.log(`ACTUAL URL: ${_url}`);
-//         }
-
-//         cy.wrap(_url).should('contain', urlPartnerSlug);
-//     })
-
-//     if (_returnToOriginalUrl) {
-//         // Return to the original url to check the other relevant links.
-//         cy.visit(_targetUrlObject.url);
-//     }
-// }
-
-
+///////////////////////
+//////// TESTS ////////
+///////////////////////
 describe('Check all application links.', () => {
     if (baseUrl) {
         before('Gather URLs', () => {
@@ -201,7 +85,7 @@ describe('Check all application links.', () => {
         })
 
         // PARTNER page tests.
-        let testType = "PARTNER";
+        let testType = "PARTNER"
         for (let i :number = 0; i < partnerPageSampleSize; i++) {
             it(`Checking ${testType} page: ${i}`, () => {
                 const urlObject :companyUrlObject = partnerUrlObjects[i];
